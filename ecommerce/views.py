628ee198ebb9a7d1e2093694e4bc0cbca4ad1db8
CarlosAntonio76 <carlosantoniodapaixao@gmail.com>
2021-05-25 15:34:40 -0300

    put imports e insert em urlpatterns o about_pag e contact_pag em src/urls.py e atualizacao no .gitignore

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,4 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Pagina principal"
+        "content": "Bem-vindo a pagina principal"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Pagina sobre"
+                 "content": "Bem-vindo a pagina principal"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Pagina contato"
+                 "content": "Bem-vindo a pagina principal"
+    }
+    return render(request, "home_page.html", context)
